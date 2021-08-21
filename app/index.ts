1b2827c016440425562e89a0161dd747edc82cdb
Luku <luku.3659@gmail.com>
2021-08-22 01:20:00 +0900

    文字盤の動作を実装。

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+import document from 'document';
+import clock, { TickEvent } from 'clock';
+
+// ----- Clock ------
+const hourHand = document.getElementById('hourHand') as GroupElement;
+const minuteHand = document.getElementById('minuteHand') as GroupElement;
+
+clock.granularity = 'minutes';
+clock.ontick = (event: TickEvent) => {
+    hourHand.groupTransform.rotate.angle =
+        30 * (event.date.getHours() % 12) + 0.5 * event.date.getMinutes();
+    minuteHand.groupTransform.rotate.angle =
+        6 * event.date.getMinutes() + 0.1 * event.date.getSeconds();
+};
+
+// ----- Clock End -----
